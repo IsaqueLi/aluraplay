64f7f2b65527bdf2d5ac84d6c337d7981ee77b36
IsaqueLi <isakilica16@gmail.com>
2023-05-26 15:54:03 -0300

    corrigindo bugs na requisicao

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -6,10 +6,10 @@ async function listaVideos() {
 } 
 
 async function criaVideo(titulo, descricao, url, imagem) {
-    const conexao = await fetch("https://localhost:3000/videos", {
+    const conexao = await fetch("http://localhost:3000/videos", {
         method: "POST",
         headers: {
-            "Content-type": "application-json"
+            "Content-type": "application/json"
         },
         body: JSON.stringify({
             titulo: titulo,
